c43cbbd2d0e505b6d45ae4e7ea9bdc073ee7bb2a
Frederik Kvartborg <fka@skemax.dk>
2016-05-28 02:00:25 +0200

    Setup mongo with travis

--- test/index.js
+++ test/index.js
@@ -3,15 +3,21 @@
 const assert = require('assert');
 const Database = require('../');
 
-describe('# Create a new instance of database', () => {
-  const DB = new Database({
-    host: '127.0.0.1',
-    port: 27017,
-    database: 'mydb'
-  });
 
+const DB = new Database({
+  host: '127.0.0.1',
+  port: 27017,
+  username: 'specla',
+  password: 'password',
+  database: 'mydb'
+});
+
+
+describe('# Create a new instance of database', () => {
   it('Default driver should be Mongo', () => assert.equal('Mongo', DB.constructor.name));
   it('Host should be 127.0.0.1', () => assert.equal('127.0.0.1', DB.options.host));
   it('Port should be 27017', () => assert.equal(27017, DB.options.port));
+  it('Username should be specla', () => assert.equal('specla', DB.options.username));
+  it('Password should be password', () => assert.equal('password', DB.options.password));
   it('Database should be mydb', () => assert.equal('mydb', DB.options.database));
 });
